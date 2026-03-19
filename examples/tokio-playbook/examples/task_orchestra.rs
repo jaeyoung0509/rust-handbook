117df60942855d1dcdc90e394585de25c2e50680
jaeyoung <43835297+jaeyoung0509@users.noreply.github.com>
2026-03-19 16:21:15 +0000

    Deepen Part 4 and Part 5 examples (#4)

--- examples/tokio-playbook/examples/task_orchestra.rs
+++ examples/tokio-playbook/examples/task_orchestra.rs
@@ -1,12 +1,17 @@
-use tokio_playbook::{collect_streaming_messages, first_completed_label, run_job_fanout};
+use tokio_playbook::{
+    cancel_slow_work, collect_streaming_messages, first_completed_label, run_job_fanout,
+    run_job_fanout_with_limit,
+};
 
 // #region tokio-main
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let jobs = run_job_fanout(vec!["borrow", "lifetime", "tokio"]).await;
+    let limited = run_job_fanout_with_limit(vec!["borrow", "lifetime", "tokio"], 1).await;
     let messages = collect_streaming_messages(vec!["read", "compile"]).await;
     let winner = first_completed_label().await;
+    let cancelled = cancel_slow_work(20, 1).await;
 
-    println!("{jobs:?} / {messages:?} / {winner}");
+    println!("{jobs:?} / {limited:?} / {messages:?} / {winner} / {cancelled}");
 }
 // #endregion tokio-main
